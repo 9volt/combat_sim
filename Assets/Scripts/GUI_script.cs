47bf6cfbb5ada55729173d99658275fcf9ed7132
chrysothemis <sonya.utecht@gmail.com>
2013-02-13 15:55:55 -0600

    Added more gui elements and healthbars. Ship cant lose health yet, but you can pause the game and change the health value to see.

--- Assets/Scripts/GUI_script.cs
+++ Assets/Scripts/GUI_script.cs
@@ -5,6 +5,12 @@ public class GUI_script : MonoBehaviour {
 	public Ship_go target;
 	public Texture img1;
 	public Texture img2;
+	public Texture lcanon;
+	public Texture rcanon;
+	public Texture helm;
+	public Texture eng;
+	public Texture healthcolor1;
+	public Texture healthcolor2;
 	//public GUISkin myskin;
 	
 	
@@ -20,13 +26,34 @@ public class GUI_script : MonoBehaviour {
     void OnGUI()
     {
 		//GUI.skin = myskin;
-		//GUI.DrawTexture(new Rect(10,10,60,70), img1);
-			
+		GUI.DrawTexture(new Rect(50,220,100,100), lcanon);
+		GUI.DrawTexture(new Rect(255,220,100,100), rcanon);
+		GUI.DrawTexture(new Rect(55,20,100,100), eng);
 		
-		if(GUI.Button(new Rect(20,40,60,70), img1)) {
+		//GUI.Box(new Rect(10,10,400,400),"" );
+		GUI.Box(new Rect(10,10,200,200),"" ); //top left
+		GUI.Box(new Rect(210,10,200,200), helm); //top right
+		GUI.Box(new Rect(210,210,200,200),"" ); //bottom right
+		GUI.Box(new Rect(10,210,200,200),"" ); //bottom left
+		
+		//Healthbars
+		GUI.DrawTexture(new Rect(50,150,target.max_health_e,20), healthcolor1);
+		GUI.DrawTexture(new Rect(50,150,target.health_e,20), healthcolor2);
+	
+		GUI.DrawTexture(new Rect(50,350,target.max_health_lc,20), healthcolor1);
+		GUI.DrawTexture(new Rect(50,350,target.health_lc,20), healthcolor2);
+		
+		GUI.DrawTexture(new Rect(250,350,target.max_health_rc,20), healthcolor1);
+		GUI.DrawTexture(new Rect(250,350,target.health_rc,20), healthcolor2);
+		
+		GUI.DrawTexture(new Rect(250,150,target.max_health_h,20), healthcolor1);
+		GUI.DrawTexture(new Rect(250,150,target.health_h,20), healthcolor2);
+		
+		
+		if(GUI.Button(new Rect(210,140,60,70), img1)) {
 			target.Turn_Ship(1);
 		}
-		if(GUI.Button(new Rect(100,40,60,70), img2)) {
+		if(GUI.Button(new Rect(350,140,60,70), img2)) {
 			target.Turn_Ship(1);
 		}
     }
--- Assets/Scripts/Ship_go.cs
+++ Assets/Scripts/Ship_go.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 using System.Collections;
 
 public class Ship_go : MonoBehaviour {
-
+	public int max_health_e = 100;
+	public int max_health_lc = 100;
+	public int max_health_rc = 100;
+	public int max_health_h = 100;
+	public int health_e ;
+	public int health_lc;
+	public int health_rc;
+	public int health_h;
+	
 	// Use this for initialization
 	void Start () {
 		rigidbody.AddForce(transform.forward  * 55);
+		health_e = max_health_e;
+		health_lc = max_health_lc;
+		health_rc = max_health_rc;
+		health_h = max_health_h;
 	}
 	
 	// Update is called once per frame
